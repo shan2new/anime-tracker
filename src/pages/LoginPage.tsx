4645a5086eb1f1e9340c5a22c2702fa6aa6783a2
Shantanu Sinha <shantanusinha95@gmail.com>
2025-03-29 16:41:01 +0530

    feat: Theme overhaul

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Session from "supertokens-web-js/recipe/session";
 import { signIn } from "supertokens-auth-react/recipe/emailpassword";
+import { Input } from "@/components/ui/input";
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
@@ -50,17 +51,17 @@ const LoginPage: React.FC = () => {
       style={{ backgroundImage: "url('/animetracker_bg.png')" }}
     >
       <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="xs:my-12 relative z-10 md:w-full max-w-md space-y-8 p-10 bg-white bg-opacity-20 backdrop-blur-md rounded-lg shadow-lg animate-fadeIn">
+      <div className="xs:my-12 relative z-10 md:w-full max-w-md space-y-8 p-10 bg-opacity-20 backdrop-blur-3xl rounded-lg shadow-lg animate-fadeIn">
         <div className="flex justify-center">
           <img src="/login_logo.png" alt="Anime Tracker Logo" className="h-64 w-auto" />
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-          <div className="rounded-md shadow-sm -space-y-px">
+          <div className="rounded-md space-y-2">
             <div>
               <label htmlFor="email-address" className="sr-only">
-                Email address
+                Email
               </label>
-              <input
+              <Input
                 id="email-address"
                 name="email"
                 type="email"
@@ -68,15 +69,15 @@ const LoginPage: React.FC = () => {
                 required
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="appearance-none rounded-t-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-orange-500 focus:border-orange-500 focus:z-10 sm:text-sm transition-all duration-300"
-                placeholder="Email address"
+                className="text-sm transition-all duration-300"
+                placeholder="email"
               />
             </div>
             <div>
               <label htmlFor="password" className="sr-only">
                 Password
               </label>
-              <input
+              <Input
                 id="password"
                 name="password"
                 type="password"
@@ -84,8 +85,8 @@ const LoginPage: React.FC = () => {
                 required
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="appearance-none rounded-b-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-orange-500 focus:border-orange-500 focus:z-10 sm:text-sm transition-all duration-300"
-                placeholder="Password"
+                className="text-sm transition-all duration-300"
+                placeholder="password"
               />
             </div>
           </div>
